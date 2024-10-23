c0897d2a725729b92972e1591a3eab5813c94990
jjg-git <john_joseph_giron@dlsu.edu.ph>
2024-10-23 19:09:09 +0800

    Added sample charts in each query

--- public/javascript/test-chart.js
+++ public/javascript/test-chart.js
@@ -2,35 +2,37 @@ var options = {
   series: [{
     name: "Desktops",
     data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-}],
+  }],
   chart: {
-  height: 350,
-  type: 'line',
-  zoom: {
-    enabled: false
-  }
-},
-dataLabels: {
-  enabled: false
-},
-stroke: {
-  curve: 'straight'
-},
-title: {
-  text: 'Product Trends by Month',
-  align: 'left'
-},
-grid: {
-  row: {
-    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-    opacity: 0.5
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
   },
-},
-xaxis: {
-  categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-}
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: '(SAMPLE) Product Trends by Month',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+  }
 };
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-chart.render();
+for (let i = 0; i <= 6; i++) {
+  var chart = new ApexCharts(document.querySelector("#chart" + i), options);
+  chart.render();
+}
 console.log(chart);
